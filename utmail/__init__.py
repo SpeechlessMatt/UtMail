22ec62d5f8e6376ec897dcf606a44d5305b6b8c1
SpeechlessMatt <115862375+SpeechlessMatt@users.noreply.github.com>
2024-11-22 21:46:58 +0800

    0.1.6 fixed undefined

--- utmail/__init__.py
+++ utmail/__init__.py
@@ -1,8 +1,14 @@
 from .utmail import UtMail
 from .utmail import Api
-from .api import *
+from . import __path__
+import os
 
-__version__ = "0.1.5"
+# 额外的模块目录 Api
+Api_path = os.path.join(os.path.dirname(__file__), 'api')
+__path__.append(Api_path)
+
+
+__version__ = "0.1.6"
 
 __all__ = [
     "UtMail",
